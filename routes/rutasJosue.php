60f9ff9071a950c2dfa6eb78547b628d4779d093
Josue Rivera <122042206@upq.edu.mx>
2024-11-01 19:13:52 -0600

    vistas front

--- routes/rutasJosue.php
+++ routes/rutasJosue.php
@@ -8,7 +8,20 @@ use App\Http\Controllers\EjerciciosPController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/rutinasgratis', [RutinasController::class, 'rutinasgratis'])->name('rutinasgratis');
-    Route::get('/ejerciciospremium', [RutinasController::class, 'ejerciciosp'])->name('ejerciciosp');
+    
 });
+// Route::get('/ejerciciospremium', [RutinasController::class, 'ejerciciosp'])->name('ejerciciosp');
 
-require __DIR__.'/auth.php';
+Route::get('/ejerciciospremium', function () {
+    return view('ejerciciospremium');
+})->name('ejerciciospremium');
+
+
+Route::get('/tipos', function () {
+    return view('tipos');
+})->name('tipos');
+
+
+Route::get('/perfilusuario', function () {
+    return view('perfilusuario');
+})->name('perfilusuario');
