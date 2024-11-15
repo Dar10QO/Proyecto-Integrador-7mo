71126d6d432266a893ef18baff352e697a918979
Josue Rivera <122042206@upq.edu.mx>
2024-11-14 18:00:19 -0600

    migraciones pi

---
+++ database/migrations/2024_11_14_015920_create_ingredients_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('calories');
+            $table->foreignId('dieta_id')->constrained('dietas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredients');
+    }
+};
